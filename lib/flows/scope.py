b357430a688c11f2d9d25a499e1d5d5ee85ac755
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2021-06-02 22:46:24 -0400

    flow(set) scope supports 'domains', allowing packages to store variables in domains with matching names to create a private (by convention) variable space

--- lib/flows/scope.py
+++ lib/flows/scope.py
@@ -7,20 +7,31 @@ class Scope:
     @property
     def parent(self): return self._parent
 
-    def has_local(self, key): return key in self._values.keys()
-    def has(self, key): return self.has_local(key) or (self.parent.has(key) if self.parent else False)
-    def get_local(self, key): return self._values[key]
-    def get(self, key): 
-        if self.has_local(key): return self.get_local(key)
-        if self.parent and self.parent.has(key): return self.parent.get(key)
-        raise Exception("Undefined: '%s'" % key)
-    def set(self, key, value, claim=False):
-        if claim and not self.claim(key, initial=value):
-            raise Exception("Claim failed for key {}".format(key))
-        self._values[key] = value
-    def claim(self, key, initial=None):
-        if not self.has_local(key):
-            self.set(key, initial, claim=False)
+    def domain(self, domain):
+        if not domain in self._values:
+            self._values[domain] = {}
+        return self._values[domain]
+
+    def has_local(self, key, domain=None): return key in self.domain(domain).keys()
+    def has(self, key, domain=None): return self.has_local(key, domain) or (self.parent.has(key, domain) if self.parent else False)
+    def get_local(self, key, domain=None): return self.domain(domain)[key]
+    def get(self, key, domain=None): 
+        if self.has_local(key, domain): return self.get_local(key, domain)
+        if self.parent and self.parent.has(key, domain): return self.parent.get(key, domain)
+        errmsg = "Undefined: '{}'".format(key)
+        if domain:
+            errmsg += " from domain {}".format(domain)
+        raise Exception(errstd)
+    def set(self, key, value, domain=None, claim=False):
+        if claim and not self.claim(key, domain=domain, initial=value):
+            errmsg = "Claim failed for key {}".format(key)
+            if domain:
+                errmsg += " in domain {}".format(domain)
+            raise Exception(errmsg)
+        self.domain(domain)[key] = value
+    def claim(self, key, domain=None, initial=None):
+        if not self.has_local(key, domain):
+            self.set(key, initial, domain=domain, claim=False)
             return True
         else:
             return False
@@ -35,7 +46,7 @@ class NamespacedScope(Scope):
         else:
             raise Exception("Unrecognised Key {}".format(key))
         
-    def has_local(self, key):
+    def has_local(self, key, domain=None):
         keys = self.parse(key)
         head = keys[0]
         tail = keys[1:]
@@ -43,41 +54,41 @@ class NamespacedScope(Scope):
         if len(keys) > 1:
             #Get the first item in this key chain and make sure it's 
             #a Scope so we can traverse into it w/ remaining keys
-            if not super().has_local(head): return False
-            scope = super().get_local(head)
+            if not super().has_local(head, domain): return False
+            scope = super().get_local(head, domain)
             if not isinstance(scope, NamespacedScope): return False
-            return scope.has_local(tail)
+            return scope.has_local(tail, domain)
         elif len(keys) == 1:
-            return super().has_local(head)
+            return super().has_local(head, domain)
         else:
             return False
         
-    def get_local(self, key):
-        if not self.has_local(key):
+    def get_local(self, key, domain=None):
+        if not self.has_local(key, domain):
             raise Exception("Undefined: '%s'" % key) 
         keys = self.parse(key)
         head = keys[0]
         tail = keys[1:]
         
         if len(keys) > 1:
-            scope = super().get_local(head)
-            return scope.get_local(tail)
+            scope = super().get_local(head, domain)
+            return scope.get_local(tail, domain)
         elif len(keys) == 1:
-            return super().get_local(head)
+            return super().get_local(head, domain)
         else:
             raise Exception("Undefined: '%s'" % key)
             
-    def set(self, key, value, claim=False):
+    def set(self, key, value, domain=None, claim=False):
         keys = self.parse(key)
         head = keys[0]
         tail = keys[1:]
         
         if len(keys) > 1:
-            if not super().has_local(head):
+            if not super().has_local(head, domain):
                 raise Exception("Undefined: {}".format(head))
-            scope = super().get_local(head)
-            scope.set(tail, value, claim)
+            scope = super().get_local(head, domain)
+            scope.set(tail, value, domain=domain, claim=claim)
         elif len(keys) == 1:
-            super().set(head, value, claim)
+            super().set(head, value, domain=domain, claim=claim)
         else:
             raise Exception("Undefined: '%s'" % key)
