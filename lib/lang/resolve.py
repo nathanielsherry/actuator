9d08a3cd2d5baea4d99dc9d889d881b6d12c4968
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2021-06-19 10:33:40 -0400

    move the last of the resolve parser components into lang.parser, replacing the old parser code

--- lib/lang/resolve.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def parse(expression):
-    from actuator.flows import flowset as mod_flowset
-    from actuator.lang import flows as flowsparser
-    flows = flowsparser.PS_FLOWSET_EXPRESSION.parseString(expression)
-    flowset = mod_flowset.FlowSet(flows)
-    return flowset
-
