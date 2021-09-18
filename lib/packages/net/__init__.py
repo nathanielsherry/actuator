507a6491545a1bca515d249794f63f62c2308e35
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2021-09-17 23:39:32 -0400

    Add net.pingable source

--- lib/packages/net/__init__.py
+++ lib/packages/net/__init__.py
@@ -4,5 +4,6 @@ def load():
     from actuator import package
     pkg = package.Package('net')
     pkg.sources.register_item('url', sources.URLSource)
+    pkg.sources.register_item('pingable', sources.Pingable)
     pkg.sinks.register_item('serve', sinks.WebServerSink)
     return pkg
--- lib/packages/net/sources.py
+++ lib/packages/net/sources.py
@@ -1,5 +1,7 @@
 from actuator import util
 from actuator.components import source
+from actuator.components.decorators import parameter, argument, input, output, allarguments
+
 
 class URLSource(source.Source):
     def initialise(self, *args, **kwargs):
@@ -14,3 +16,16 @@ class URLSource(source.Source):
             from html2text import html2text
             result = html2text(result)
         return result
+
+@output('bool', 'True if-and-only-if pingable')
+@argument('address', 'str', '8.8.8.8', 'Address to ping')
+class Pingable(source.Source):
+    """
+    Tests if an address is pingable. Emits boolean True if it is, and False otherwise.
+    """
+    @property
+    def value(self):
+        import subprocess
+        result = subprocess.run(['ping', '-c', '3', self.args.accessor])
+        return result.recurncode == 0
+
