96e771e26d75f5d5c0c8420fcb088dba637cf029
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2021-10-11 14:37:01 -0400

    update fmt package w/ decorators

--- lib/packages/fmt/operators.py
+++ lib/packages/fmt/operators.py
@@ -1,22 +1,26 @@
 from actuator import util
 from actuator.components import operator
 
+from actuator.components.decorators import parameter, argument, input, output, allarguments, operator
+
+
+@input('any', 'Any payload')
+@output('str', 'JSON String')
+@parameter('pretty', 'bool', False, 'Apply formatting to JSON output')
 class ToJson(operator.Operator):
-    def initialise(self, *args, **kwargs):
-        super().initialise(*args, **kwargs)
-        self._pretty = util.parse_bool(kwargs.get('pretty', 'true'))
-    
     @property
     def value(self):
         import json
         value = self.upstream.value
         if value == None: return None
-        if self._pretty:
+        if self.params.pretty:
             return json.dumps(value, indent=4)
         else:
             return json.dumps(value)
 
 
+@input('str', 'JSON String')
+@output('any', 'Parsed payload')
 class FromJson(operator.Operator):
     @property
     def value(self):
@@ -27,27 +31,33 @@ class FromJson(operator.Operator):
         return value
         
         
-        
+
+@input('any', 'Any payload')
+@output('str', 'YAML String')
+@parameter('unsafe', 'bool', False, 'Use unsafe PyYAML Dumper')
+@parameter('canonical', 'bool', False, 'Dump YAML in canonical format with explicit types')
+@parameter('default_flow_style', 'bool', True, "PyYAML's 'default_flow_style' argument")
 class ToYaml(operator.Operator):
-    def initialise(self, *args, **kwargs):
-        super().initialise(*args, **kwargs)
-        self._pretty = util.parse_bool(kwargs.get('pretty', 'true'))
-    
     @property
     def value(self):
         import yaml
         value = self.upstream.value
         if value == None: return None
-        if self._pretty:
-            return yaml.dump(value)
+        if not self.params.unsafe:
+            return yaml.safe_dump(value, canonical=self.params.canonical)
         else:
-            return yaml.dump(value)
+            return yaml.dump(value, canonical=self.params.canonical)
 
-
+@input('str', 'YAML String')
+@output('any', 'Any payload')
+@parameter('unsafe', 'bool', False, 'Use unsafe PyYAML Dumper')
 class FromYaml(operator.Operator):
     @property
     def value(self):
         import yaml
         value = self.upstream.value
         if value == None: return None
-        return yaml.load(value)
+        if self.params.unsafe:
+            return yaml.load(value)
+        else:
+            return yaml.safe_load(value)
