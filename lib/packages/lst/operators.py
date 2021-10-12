54f4a1d5116eb781e9ae5e77ff86e306b717a45e
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2021-10-11 23:06:22 -0400

    Updated lst module with function operator style, arg/param decorators

--- lib/packages/lst/operators.py
+++ lib/packages/lst/operators.py
@@ -1,125 +1,69 @@
 from actuator import util
-from actuator.components import operator
+from actuator.components.operator import Operator 
+
+from actuator.components.decorators import parameter, argument, input, output, allarguments, operator
+
+@operator
+def head(payload): return payload[0]   
+
+@operator
+def tail(payload): return payload[1:]
+
+@operator
+def last(payload): return payload[-1]
+
+@operator
+def init(payload): return payload[:-2]   
+
+#def argument(name, ptype, default=None, desc=None, parser=None):
+@argument('start', 'int', 0, 'Starting index for slice. Inclusive')
+@argument('end', 'int', -1, 'Ending index for slice. Exclusive')
+@operator
+def slice(payload, start=0, end=-1):
+    return payload[start:end]
+
+@operator
+def length(payload): return len(payload)
+
+@operator
+def reverse(payload):
+    payload = payload[:]
+    payload.reverse()
+    return payload
+
+@parameter('delim', 'str', ', ', 'Delimiter to join with')
+@operator
+def join(payload, delim=', '): return delim.join(payload)
+
+@operator
+def lst_sum(payload): return sum(payload)
+
+@operator
+def avg(payload): return float(sum(payload))/float(len(payload))
 
 
-class Head(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return value[0]
-    
-    
-class Tail(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return value[1:]
-    
-    
-class Last(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return value[-1]
-    
-    
-class Init(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return value[:-2]
-    
-class Slice(operator.Operator):
-    def initialise(self, *args, **kwargs):
-        super().initialise(*args, **kwargs)
-        self._start = args[0]
-        self._end = args[1]
-        
-    @property
-    def value(self):
-        value = self.upstream.value
-        return value[self._start:self._end]
-    
-    
-class Length(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return len(value)
-        
-class Reverse(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value[:]
-        value.reverse()
-        return value
-    
-class Join(operator.Operator):
-    def initialise(self, *args, **kwargs):
-        super().initialise(*args, **kwargs)
-        self._delim = args[0]
-        
-    @property
-    def value(self):
-        value = self.upstream.value
-        return self._delim.join(value)
-        
-class Sum(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return sum(value)
-        
-class Avg(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return float(sum(value))/float(len(value))
-        
-class Prod(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        p = 1
-        for v in value:
-            p *= v
-        return p
-    
-class Max(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return max(value)
-        
-class Min(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return min(value)
-         
-        
-class Replicate(operator.Operator):
-    def initialise(self, *args, **kwargs):
-        super().initialise(*args, **kwargs)
-        self._times = int(args[0])
-        
-    @property
-    def value(self):
-        value = self.upstream.value
-        agg = []
-        for i in range(0, self._times):
-            agg.append(value)
+@operator
+def product(payload):
+    p = 1
+    for v in payload:
+        p *= v
+    return p
+
+@operator
+def lst_max(payload): return max(payload)
+
+@operator
+def lst_min(payload): return min(payload)
+
+
+@argument('count', 'int', 1, 'Number of repetitions', parser=int)
+@operator
+def repeat(payload, count=1): return [payload for i in range(0, count)]
             
 
 #TODO: nice if there were a more generic way to map a function over a list
-class Ints(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return [int(s) for s in value]
+@operator
+def ints(upstream): return [int(s) for s in upstream]
     
-class Floats(operator.Operator):
-    @property
-    def value(self):
-        value = self.upstream.value
-        return [float(s) for s in value]
+@operator
+def floats(upstream): return [float(s) for s in upstream]
