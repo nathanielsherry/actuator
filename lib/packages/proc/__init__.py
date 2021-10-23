a73ee604bff094a317e63e454fa3cd66d1bcfa67
Nathaniel Sherry <nathaniel.sherry@gmail.com>
2021-10-23 10:02:01 -0400

    Modernised the proc package

--- lib/packages/proc/__init__.py
+++ lib/packages/proc/__init__.py
@@ -4,10 +4,11 @@ def load():
     from actuator import package
     pkg = package.Package('proc')
     
-    pkg.sources.register_item(None, sources.Info)
-    pkg.sources.register_item('get', sources.Get)
-    pkg.sources.register_item('has', sources.Has)
-    pkg.sources.register_item('names', sources.Names)
+    pkg.sources.register_item(None, sources.info)
+    pkg.sources.register_item('get', sources.get)
+    pkg.sources.register_item('has', sources.has)
+    pkg.sources.register_item('search', sources.search)
+    pkg.sources.register_item('names', sources.names)
     #pkg.sources.register_item('stdin', sources.StdinSource)
     #pkg.sources.register_item('jsonin', sources.JsonSource)
     #pkg.sinks.register_item(None, sinks.ShellRunner)
--- lib/packages/proc/sources.py
+++ lib/packages/proc/sources.py
@@ -3,63 +3,67 @@ import psutil
 from actuator.components import source
 from actuator import util
 
-class Info(source.Source):
-    def initialise(self, *args, **kwargs):
-        super().initialise(*args, **kwargs)
-        self._full = util.parse_bool(kwargs.get('full', 'false'))
-        self._keys = args
-        
-    @property
-    def value(self):
-        if self._full:
-            keys = list(psutil.Process().as_dict().keys())
-        else:
-            keys = ['pid', 'name', 'username', 'cmdline', 'cpu_percent', 'uids', 'gids', 'nice', 'status', 'cwd']
-            if self._keys: keys.extend(self._keys)
-            keys = list(set(keys))
-        processes = []
-        for proc in psutil.process_iter(keys):
-            processes.append(proc.as_dict(attrs=keys))
-        return processes
+
+from actuator.components.decorators import parameter, argument, input, output, allparameters, source
+
+
+def ps(keys=None):
+    if not keys:
+        keys = list(psutil.Process().as_dict().keys())
+    processes = []
+    for proc in psutil.process_iter(keys):
+        processes.append(proc.as_dict(attrs=keys))
+    return processes
+
+
+@output('list[dict{str: str}]', 'Information about running processes')
+@argument('keys', 'list[str]', ['pid', 'name', 'username', 'cmdline', 'cpu_percent', 'uids', 'gids', 'nice', 'status', 'cwd'], 'Properties to access, returns all properties by default.')
+@source
+def info(keys):
+    return ps(keys)
+
+@output('list[str]', 'Specific value for running processes')
+@argument('key', 'str', None, 'The name of the property to access')
+@source
+def get(key):
+    return [p[key] for p in ps([key] if key else None)]        
+
+
+@output('list[str]', 'Names of running processes')
+@source
+def names(self):
+    return [p['name'] for p in ps(['name'])]
 
 
 
-class Get(Info):
-    def initialise(self, *args, **kwargs):
-        super().initialise(*args, **kwargs)
-        self._key = args[0]
-        
-    @property
-    def value(self):
-        value = super().value
-        return [p[self._key] for p in value]        
-
-class Names(Info):      
-    @property
-    def value(self):
-        value = super().value
-        return [p['name'] for p in value]
+def do_search(search, keys):
+    search_keys = list(search.keys())
     
-class Has(source.Source):
-    def initialise(self, *args, **kwargs):
-        super().initialise(*args, **kwargs)
-        self._search = kwargs
-    
-    def compare(self, proc):
-        keys = list(self._search.keys())
-        d = proc.as_dict(attrs=keys)
-        for key in keys:
-            if d[key] != self._search[key]:
+    def check(entry):
+        for key in search_keys:
+            if entry[key] != search[key]:
                 return False
         return True
     
-    @property
-    def value(self):
-        processes = []
-        keys = list(self._search.keys())
-        if len(keys) == 0: return False
-        procs = psutil.process_iter(keys)
-        for proc in procs:
-            if self.compare(proc): return True
-        return False
-                            
+    processes = []
+    all_keys = list(set(keys + search_keys))
+    if len(all_keys) == 0: return []
+    data = ps(all_keys)
+    data = [d for d in data if check(d)]
+    return data
+
+@output('list[str]', 'Information for matching processes')
+@argument('keys', 'list[str]', ['pid', 'name', 'username', 'cmdline', 'cpu_percent', 'uids', 'gids', 'nice', 'status', 'cwd'], 'Properties to access, returns all properties by default.')
+@allparameters('search')
+@source
+def search(keys, search):
+    return do_search(search, keys)
+                 
+
+@output('bool', 'True if there are matching processes, False otherwise')
+@allparameters('search')
+@source
+def has(search):
+    data = do_search(search, [])
+    return len(data) > 0
+                        
